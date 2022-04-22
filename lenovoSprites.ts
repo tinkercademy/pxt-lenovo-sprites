1b110093a0e831ab5485db020a384bfe2a31b203
Xenia Fiorenza <xenia.anong@gmail.com>
2022-04-22 15:11:30 +0800

    add sprites

---
+++ lenovoSprites.ts
@@ -0,0 +1,16 @@
+namespace images.lenovoSprites {
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoMascotFront = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoMascotRight = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoMascotLeft = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoMascotBack = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesBubbleTea = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoGrassTile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const lenovoSpritesLenovoGrassTile2 = image.ofBuffer(hex``);
+}
